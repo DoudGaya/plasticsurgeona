e20cd5184395ddf23f4c948095913eb964e39519
DoudGaya <arbdoule@yahoo.com>
2023-08-05 22:17:35 +0100

    git

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -8,7 +8,7 @@ import { useGlobanNavContext } from './context/MobileNavContext'
 
 export const MobileNav = () => {
 
-    const { navSwitcher }: any = useGlobanNavContext()
+    const { navSwitcher, nav }: any = useGlobanNavContext()
     
   return (
     <div className=' w-full justify-between flex lg:hidden px-8 py-4 bg-secondary border-b z-40 border-primary shadow-lg sticky left-0 top-0'>
@@ -17,9 +17,18 @@ export const MobileNav = () => {
           </Link>   
 
           <button onClick={() => {navSwitcher()}}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
-            </svg>
+              {nav ?
+                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  :
+                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+                  </svg>
+                }
+
+             
+
           </button>
     </div>
   )
